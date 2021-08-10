ff4324d3d52ca70d3c87494cfdc1c39a1dfa931c
Niko <nz76@kent.ac.uk>
2021-08-10 19:48:57 +0100

    Explore screen added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import ExploreScreen from "./src/screens/ExploreScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Explore: ExploreScreen,
   },
   {
     initialRouteName: "Home",
---
+++ src/screens/ExploreScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ExploreScreen = () => {
+  return <Text style={styles.text}>Explore</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default ExploreScreen;

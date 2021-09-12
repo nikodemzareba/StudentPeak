8345706f350e128bdb0266e87eec7a63db18beb7
Niko <nz76@kent.ac.uk>
2021-09-12 02:13:06 +0100

    Welcome Page

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import Welcome from './components/authentication/Welcome';
+
+// Creates Route. Landing Page -> Login -> Register page
+const Stack = createStackNavigator(); 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Welcome">
+        <Stack.Screen name ="Welcome" component={Welcome} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ components/authentication/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Text, View, Button} from 'react-native'
+
+export default function Welcome({navigation}) {
+    return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+
+            <Button 
+            title = "Register" 
+            onPress= {() => navigation.navigate("Register")}
+            />
+            <Button 
+            title = "Login" 
+            onPress= {() => navigation.navigate("Login")}
+            />
+        </View>
+    )
+}

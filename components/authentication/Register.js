3990b2bf0958cadd579cd49e2fb660114dcaa42b
Niko <nz76@kent.ac.uk>
2021-09-12 02:53:13 +0100

    Register function

---
+++ components/authentication/Register.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native'
+
+export class Register extends Component {
+    constructor(props){
+    super (props);
+    this.state = {
+        email: '',
+        password: '',
+        name: ''
+       }
+       this.onSignUp = this.onSignUp.bind(this)
+    }
+
+    onSignUp(){
+        
+
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="name"
+                    onChangeText={(name) => this.setState({name})}
+                />
+
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({email})}
+                />
+
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true} 
+                    onChangeText={(password) => this.setState({password})}
+                />
+                <Button
+                  onPress={() => this.onSignUp()}
+                  title="Sign Up"
+                />
+            </View>
+        )
+    }
+}
+
+
+export default Register

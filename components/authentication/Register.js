9179f77164a8526d6ec42c5de5808933c8f6f215
Aleksej Bratkovskij <ab2323@kent.ac.uk>
2021-10-31 20:00:28 +0000

    updated Register.js so that it includes creation of bio, friends, messages, photos and topics fields for future use

--- components/authentication/Register.js
+++ components/authentication/Register.js
@@ -8,25 +8,33 @@ import "firebase/firestore";
 export class Register extends Component {
     constructor(props){
     super (props);
-
-    this.state = {
-        email: '',
-        password: '',
-        name: ''
-       }
-       this.onSignUp = this.onSignUp.bind(this)
+        this.onSignUp = this.onSignUp.bind(this)
     }
 
     onSignUp(){
-        const { email, password, name} = this.state;
+        const  {email,
+                password,
+                name,
+                bio,
+                topics,
+                friends,
+                photos,
+                messages
+                } = this.state;
+
         firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((result) => {
             firebase.firestore().collection("users")
             .doc(firebase.auth().currentUser.uid)
             .set({
                 name,
-                email
-            })
+                email,
+                bio,
+                topics,
+                friends,
+                photos,
+                messages
+            },)
             console.log(result)
         })
         .catch((error) => {

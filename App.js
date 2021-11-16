01b1d51d9f8eb414c32109419921358b3f201ae4
Aleksej Bratkovskij <ab2323@kent.ac.uk>
2021-11-16 17:04:50 +0000

    saving App.js from Main

--- App.js
+++ App.js
@@ -45,9 +45,9 @@ import Bio from './components/authentication/Bio';
 const Stack = createStackNavigator();
 
 export default function App() {
-   const [loaded] = useFonts({
+  const [loaded] = useFonts({
     Montserrat: require('./assets/fonts/Montserrat.ttf'),
-   });
+  });
 
   if (!loaded) {
     return null;
@@ -55,21 +55,21 @@ export default function App() {
 
 
   return ( // Welcome screen set to first stack screen. 
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName ="Welcome"> 
-        <Stack.Screen name = "Welcome" component={Welcome} options={{ headerShown: false}} />
-        <Stack.Screen name = "Login" component={Login} options={{ headerShown: false}}/>
-        <Stack.Screen name = "Register" component={Register} options={{ headerShown: false}}/>
-        <Stack.Screen name = "Verify" component={Verify} options={{ headerShown: false}}/>
-        <Stack.Screen name = "PrivateProfile" component={PrivateProfile} options={{ headerShown: false}}/>
-        <Stack.Screen name = "PublicProfile" component={PublicProfile} options={{ headerShown: false}}/>
-        <Stack.Screen name = "AboutYou" component={AboutYou} options={{ headerShown: false}}/>
-        <Stack.Screen name = "ChooseUsername" component={ChooseUsername} options={{ headerShown: false}}/>
-        <Stack.Screen name = "StudyDetails" component={StudyDetails} options={{ headerShown: false}}/>
-        <Stack.Screen name = "Connect" component={Connect} options={{ headerShown: false}}/>
-        <Stack.Screen name = "Bio" component={Bio} options={{ headerShown: false}}/>
-       
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName ="Welcome">
+          <Stack.Screen name = "Welcome" component={Welcome} options={{ headerShown: false}} />
+          <Stack.Screen name = "Login" component={Login} options={{ headerShown: false}}/>
+          <Stack.Screen name = "Register" component={Register} options={{ headerShown: false}}/>
+          <Stack.Screen name = "Verify" component={Verify} options={{ headerShown: false}}/>
+          <Stack.Screen name = "PrivateProfile" component={PrivateProfile} options={{ headerShown: false}}/>
+          <Stack.Screen name = "PublicProfile" component={PublicProfile} options={{ headerShown: false}}/>
+          <Stack.Screen name = "AboutYou" component={AboutYou} options={{ headerShown: false}}/>
+          <Stack.Screen name = "ChooseUsername" component={ChooseUsername} options={{ headerShown: false}}/>
+          <Stack.Screen name = "StudyDetails" component={StudyDetails} options={{ headerShown: false}}/>
+          <Stack.Screen name = "Connect" component={Connect} options={{ headerShown: false}}/>
+          <Stack.Screen name = "Bio" component={Bio} options={{ headerShown: false}}/>
+
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }

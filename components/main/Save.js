e7660a065cfa366a0c4430572c444d57dc45f279
Tyrone Friday <tyrone.f2000@outlook.com>
2021-11-28 19:10:05 +0000

    webstorm features

--- components/main/Save.js
+++ components/main/Save.js
@@ -16,53 +16,7 @@ export default function Save(props) {
 
     const uri = props.route.params.image;
 
-    const uploadImage = async () => {
-
-        const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random.toString(36)}`;
-        const response = await fetch(uri);
-        const blob = await response.blob();
-
-        const task = firebase
-            .storage()
-            .ref()
-            .child(childPath)
-            .put(blob);
-
-        const taskProgress = snapshot => {
-            console.log(`transferred: ${snapshot.bytesTransferred}`)
-        }
-
-        const taskCompleted = () => {
-            task.snapshot.ref.getDownloadURL().then((snapshot) => {
-                savePostData(snapshot);
-                console.log(snapshot)
-            })
-        }
-
-        const taskError = snapshot => {
-            console.log(snapshot)
-        }
-
-        task.on.toString("state_changed", taskProgress, taskError, taskCompleted)
-    }
-
-    const savePostData = (downloadURL) => {
-
-        firebase.firestore()
-            .collection('posts')
-            .doc(firebase.auth().currentUser.uid)
-            .collection("userPosts")
-            .add({
-                downloadUrRL,
-                caption,
-                creation: firebase.firestore.FieldValue.serverTimeStamp()
-            }).then((function () {
-            //props.navigation.popToTop()  // returns screen back to main page
-
-        }))
-    }
-
-    const uploadImage2 = async () => {
+     const uploadImage2 = async () => {
         const childPath2 = `posts/${firebase.auth().currentUser.uid}/${getRandomString(36)}`;
         const response = await fetch(uri); // fetch image
         const blob = await response.blob(); // convert to blob
@@ -107,7 +61,8 @@ export default function Save(props) {
 
     return (
         <View style={{flex: 1}}>
-            <Image source={{uri: props.route.params.image}}/>
+
+            <Image source = {{uri:props.route.params.image}} style = {{flex: 1}} /> {/* Displays image taken below  */}
             <TextInput
                 placeholder="Write a Caption ...."
                 onChangeText={(caption) => setCaption(caption)}

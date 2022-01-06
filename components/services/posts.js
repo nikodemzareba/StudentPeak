7ee840186523558902f24b9393bf48fdb3abd444
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-01-06 19:45:16 +0000

    Try to implement users following to videos script

--- components/services/posts.js
+++ components/services/posts.js
@@ -2,6 +2,7 @@ import firebase from "firebase"
 
 export const getFeed = () => new Promise((resolve, reject) => {
     console.log('attempt to connect')
+    /*
     firebase
         .firestore()
         .collection('Videos')
@@ -20,5 +21,41 @@ export const getFeed = () => new Promise((resolve, reject) => {
                 console.log('got data')
             },
         )
+    console.log("should be rendering data")
+
+     */
+
+    // Get Users ID's the user is following
+    firebase.firestore()
+        .collection('following')
+        .doc(firebase.auth().currentUser.uid)
+        .collection('userFollowing')
+        .get()
+        .then(snapshot => {
+            snapshot.forEach((user) => {
+                firebase.firestore()
+                    .collection('posts')
+                    .doc(user.id)
+                    .collection('userPosts')
+                    .where("mediaType", "==", "video")
+                    .get()
+                    .then(userPost => {
+                        // Videos
+                        let posts = userPost.docs.map((value) => {
+                            const id = value.id;
+                            const data = value.data();
+                            return { id, ...data }
+                        })
+                        console.log(userPost.id)
+                        resolve(posts)
+                    })
+                    .catch((error) => {
+                        console.log(`${error} \nUnable to get Users following posts!`);
+                    });
+            })
+        })
+        .catch((error) => {
+            console.log(`${error} \nUnable to Get users Following!`);
+        });
     console.log("should be rendering data")
 })

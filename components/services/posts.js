f3e5873cfe17e9450158c05f3de2c4d451ea09e8
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-01-07 07:09:45 +0000

    Try to implement users following to videos script

--- components/services/posts.js
+++ components/services/posts.js
@@ -1,30 +1,67 @@
 import firebase from "firebase"
 
 export const getFeed = () => new Promise((resolve, reject) => {
-    console.log('attempt to connect')
-    /*
-    firebase
-        .firestore()
-        .collection('Videos')
+    // Get Users ID's the user is following
+    firebase.firestore()
+        .collection('following')
+        .doc(firebase.auth().currentUser.uid)
+        .collection('userFollowing')
         .get()
-        //get res as usable object
-        .then((res)=>{
-                //map info to object we can use
-                const post = res.docs.map((value) =>{
-                    const id = value.id;
-                    const data = value.data();
-                    //should return all the id with their objects
-                    return {id, ...data}
+        .then(snapshot => {
+            // for each user ID we are following
+            let data = [];
+            snapshot.forEach((user) => {
+                // console.log("!Users Following: " + user.id);
+                data.push(user.id)
+            })
 
-                })
-                resolve(post)
-                console.log('got data')
-            },
+            return data;
+        })
+        .catch((error) => {
+            console.log(`${error} \nUnable to Get users Following!`);
+        })
+        .then(usersFollowing =>
+            usersFollowing.forEach((userID) => {
+                firebase.firestore()
+                    .collection('posts')
+                    .doc(userID)
+                    .collection('userPosts')
+                    .where("mediaType", "==", "video")
+                    .get()
+                    .then(userPost => {
+                        console.log("\n\nUsersID: " + userID);
+                        let data2 = [];
+                        userPost.forEach((userPost) => {
+                            console.log("PostID : " + userPost.id + " | MediaType: "+ userPost.get("mediaType") + " | Download URL: " + userPost.get("downloadURL"));
+                            data2.push(userPost.id);
+                        })
+
+
+                        //########################################################################
+                        // Israel Code
+                        let posts = userPost.docs.map((value) => {
+                            const id = value.id;
+                            const data = value.data();
+                            return { id, ...data }
+                        })
+                       // console.log(userPost.id)
+                        resolve(posts)
+                    })
+                   //########################################################################
+            })
         )
-    console.log("should be rendering data")
+        .catch((error) => {
+            console.log(`${error} \nUnable to get Users following posts!`);
+        });
 
-     */
 
+
+
+
+//################################################################################################################
+//################################################################################################################
+//################################################################################################################
+/*
     // Get Users ID's the user is following
     firebase.firestore()
         .collection('following')
@@ -65,4 +102,7 @@ export const getFeed = () => new Promise((resolve, reject) => {
             console.log(`${error} \nUnable to Get users Following!`);
         });
     console.log("should be rendering data")
+ */
+
 })
+

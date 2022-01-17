d8ab5cd25c98f77d3dd03e1dc574ceb5683fb765
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-01-17 18:52:29 +0000

    Testing API

--- components/main/Events.js
+++ components/main/Events.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+
 
 export default function Events() {
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    console.log("\n\nData"+data);
+
+    useEffect(() => {
+        fetch('/api/googleEvents/Clubbing%20In%20Canterbury/United%20Kingdom')
+            .then((response) => response.json())
+            .then(async (json) => {
+                await setData(json);
+                console.log(`\n\nResults \n${json}`);
+                console.log(`\n\nTitle 1\n${json[0].title}`);
+            })
+            .catch((error) => console.error(error))
+            .finally(() => {
+                setLoading(false)
+               });
+
+
+
+        }, []);
+
     return (
-        <View>
-            {/*Shows you what page you're on */}
-            <Text>Events</Text>
+
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? <Text>Loading...</Text> :
+                ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+                        <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
+                        <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
+
+                    </View>
+                )}
         </View>
-    )
-}
+    );
+};
--- proxyServer/server.js
+++ proxyServer/server.js
@@ -3,6 +3,9 @@ const app = express();
 const fetch = require('node-fetch');
 const  cors = require('cors');
 
+let queryParam = "Clubbing In Cantebury";
+let locationParam = "United Kingdom";
+
 app.use(
 
     cors({
@@ -11,13 +14,43 @@ app.use(
 //cors()
 )
 
+//http://localhost:3000/api/googleEvents/Clubbing In Canterbury/United Kingdom
+app.get("/api/googleEvents/:query/:location",  async (req, res) => {
+    /*
+    const response = await fetch("https://raw.githubusercontent.com/adhithiravi/React-Hooks-Examples/master/testAPI.json");
+    res.json(await response.json())
+
+     */
+
+    //#######################################################
+
+    queryParam = req.params.query;
+    locationParam = req.params.location;//
+
+    const SerpApi = require('google-search-results-nodejs');
+    const search = new SerpApi.GoogleSearch("da7203eb879f74f5c81852618ca7a91c2d35a0c298af3ed1d11d14331eed437b");
+
+    const params = {
+        engine: "google_events",
+        q: `${queryParam}`,
+        hl: "en",
+        gl: "uk",
+        location:`${locationParam}`,
+        num:"100"
+    };
+    const callback = function(data) {
+       console.log(data['events_results']);
+        res.json(data['events_results'])
+       // res.json(data)
+    };
+
+    // Show result as JSON
+    search.json(params, callback);
+
+
+})
 const PORT = process.env.PORT || 3000
 
-app.get("/",  async (req, res) => {
-    const response = await fetch("https://reactnative.dev/movies.json");
-    res.json(await response.json())
-})
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}` )
 })

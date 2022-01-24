597a1e7e9fa6b64db29a0f53465706f97efb4cf4
coDe-ji <dejifasuba17@gmail.com>
2022-01-24 18:48:29 +0000

    feed screen

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { user } from './user'
 import messageReducer from './messageReducer';
+import { users } from "./users";
 
 export default combineReducers({
     userState: user,
+    usersState: users,
     messages: messageReducer,
 });

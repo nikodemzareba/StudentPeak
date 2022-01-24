3b2f7f4bb29daa09031f8b1d088973a6245e5b8d
coDe-ji <dejifasuba17@gmail.com>
2022-01-24 18:50:34 +0000

    feed

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -210,9 +210,9 @@ export function fetchUsersFollowingPosts(uid) {
             .orderBy("creation", "asc")
             .get()
             .then((snapshot) => {
+                
                 const uid = snapshot.docs[0].ref.path.split('/')[1];
                 const user = getState().usersState.users.find(el => el.uid === uid);
-
 
                 let posts = snapshot.docs.map(doc => {
                     const data = doc.data();

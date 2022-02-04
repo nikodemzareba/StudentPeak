7fb2e247fff4f04afc2e9baa8b1e127eddc15d75
Tyrone Friday <tyrone.f2000@outlook.com>
2022-02-04 04:58:44 +0000

    Feed Screen is able to store and process video data from firebase and integrate this into an  object which the data can be fetched from.
    However, the video data now just needs to be plugged into the current format to display it

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,12 +1,13 @@
 import React, {Component, useState} from 'react'
-import { View, Dimensions, FlatList, Text } from 'react-native';
+import {View, Dimensions, FlatList, Text, ActivityIndicator, ScrollView, Button} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import VideoPlayer from "./feedControl/components/VideoPlayer";
 import firebase from "firebase";
+import ListItem from "react-native-paper/src/components/List/ListItem";
 
 
-const { height, width } = Dimensions.get('window');
+const {height, width} = Dimensions.get('window');
 const DATA = require("./feedControl/data.json").videos;
 const videosData = DATA.map((item, i) => {
     return {...item, key: i}
@@ -14,81 +15,91 @@ const videosData = DATA.map((item, i) => {
 
 const data2 = [
     {
-       ty:"Hello"
+        ty: "Hello"
     },
     {
-        ty:"Hello"
+        ty: "Hello"
     }
 ];
 
-const seperator = "####################################################################################";
-console.log(`\n\n${seperator} \n\nData2${data2} \n\n${seperator}`);
-console.log(`\n\n${seperator} \n\nData2 ${data2[0].ty} \n\n${seperator}`);
+const separator = "####################################################################################";
+console.log(`\n\n${separator} \n\nData2${data2} \n\n${separator}`);
+console.log(`\n\n${separator} \n\nData2 ${data2[0].ty} \n\n${separator}`);
 
-class FeedScreen extends Component{
+class FeedScreen extends Component {
 
 
     constructor(props) {
         super(props);
+        this.docs = firebase.firestore()
+            .collection('following')
+            .doc(firebase.auth().currentUser.uid)
+            .collection('userFollowing')
         this.state = {
-            outOfBoundItems:[],
-            dataFetched: firebase.firestore()
-                .collection('following')
-                .doc(firebase.auth().currentUser.uid)
-                .collection('userFollowing')
-                .get()
-                .then(snapshot => {
-                    const data = [];
-                    snapshot.forEach((user) => {
-                        console.log(`\n\nUsersID: ${user.id}`);
-                        firebase.firestore()
-                            .collection('posts')
-                            .doc(user.id)
-                            .collection('userPosts')
-                            .where("mediaType", "==", "video")
-                            .get()
-                            .then(usersFollowingPosts => {
-                                usersFollowingPosts.forEach((userPost) => {
-                                    data.push({
-                                        userID: user.id,
-                                        postID: userPost.id,
-                                        caption: userPost.get("caption") ,
-                                        createdAt: userPost.get("createdAt"),
-                                        downloadURL: userPost.get("downloadURL")
-                                    });
-                                    console.log("\nPostID : " + userPost.id + " | MediaType: "+ userPost.get("mediaType") + " | Download URL: " + userPost.get("downloadURL"));
-                                })
-                            })
-                            .catch((error) => {
-                                console.log(`${error} \nUnable to get Users following posts!`);
-                            });
-                    })
-                    console.log(`\n\n${seperator} \n${seperator} \n\nData Retrieved  ${data} \n\n${seperator} \n${seperator}`)
-                    return data;
-                })
-                .catch((error) => {
-                    console.log(`${error} \nUnable to Get users Following!`);
-                })
+            outOfBoundItems: [],
+            dataFetched: [],
+            isLoading: true
         };
-
-        // const jsonData = this.state.dataFetched;
-        // console.log(`\n\n${seperator} \nData Retrieved Caption 1 ${jsonData[0]} \n\n${seperator}`)
     }
 
-    handleViewableItemsChanged = ({ changed }) => {
+    handleViewableItemsChanged = ({changed}) => {
 
         const outOfBoundItems = changed;
 
-        if(outOfBoundItems.length !== 0) {
+        if (outOfBoundItems.length !== 0) {
             this.setState({outOfBoundItems});
         }
 
     };
 
+    componentDidMount() {
+        this.unsubscribe = this.docs.onSnapshot(this.getData);
+        console.log("\n\nHERE")
+    }
 
-    renderPosts = ({ item, index }) => {
+    getData = (querySnapshot) => {
+        const dataFetched = [];
+        querySnapshot.forEach((user) => {
+            console.log(`\n\nUsersID: ${user.id}`);
+            firebase.firestore()
+                .collection('posts')
+                .doc(user.id)
+                .collection('userPosts')
+                .where("mediaType", "==", "video")
+                .get()
+                .then(usersFollowingPosts => {
+
+                    usersFollowingPosts.forEach((userPost) => {
+                        const {caption, createdAt, downloadURL, mediaType} = userPost.data();
+                        dataFetched.push({
+                            key: userPost.id,
+                            userID: user.id,
+                            caption,
+                            createdAt,
+                            downloadURL,
+                        });
+                        console.log(`\n\nUserID: ${user.id} \nCaption: ${caption} \nCreatedAt: ${createdAt} \nDownloadURL: ${downloadURL} \nMediaType: ${mediaType}`);
+                        console.log("\nPostID : " + userPost.id + " | MediaType: " + userPost.get("mediaType") + " | Download URL: " + userPost.get("downloadURL"));
+                    })
+
+
+                })
+                .catch((error) => {
+                    console.log(`${error} \nUnable to get Users following posts!`);
+                });
+        })
+        this.setState({
+            outOfBoundItems: [],
+            dataFetched,
+            isLoading: false
+        });
+    }
+
+
+
+    renderPosts = ({item, index}) => {
         return <VideoPlayer
-            height={height/1.6}
+            height={height / 1.6}
             width={width}
             videoUri={item.video}
             item={item}
@@ -98,8 +109,36 @@ class FeedScreen extends Component{
 
 
     render() {
+        if (this.state.isLoading) {
+            console.log(`\n\n${separator} \n\nData Fetched ${this.state.dataFetched} \n\n${separator}`);
+            return (
+                <View >
+                    <ActivityIndicator size="large" color="red"/>
+                </View>
+            )
+        }
+
+
         return (
             <View style={{flex:1}}>
+                <Button
+                    title="Press me"
+                    color="#f194ff"
+                    onPress={() => {
+                        console.log(`\n\n${separator}`);
+
+                        console.log(`\n\nData Fetched OBJECT: ${this.state.dataFetched}`);
+
+                        console.log(`\n\nFirst Object Data`);
+                        console.log(`\nKey: ${this.state.dataFetched[0].key}`);
+                        console.log(`\nuserID: ${this.state.dataFetched[0].userID}`);
+                        console.log(`\nCaption: ${this.state.dataFetched[0].caption}`);
+                        console.log(`\nCreatedAt: ${this.state.dataFetched[0].createdAt}`);
+                        console.log(`\ndownloadURL: ${this.state.dataFetched[0].downloadURL}`);
+
+                        console.log(`\n\n${separator}`);
+                    }}
+                />
                 <FlatList
                     style={{ flex:1 }}
                     contentContainerStyle={{paddingTop:25}}
@@ -113,7 +152,7 @@ class FeedScreen extends Component{
                     overScrollMode="never"
                 />
             </View>
-        )
+        );
     }
 
 }

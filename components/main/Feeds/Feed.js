fcb0e436c3f2023f57cad499c7b44fcdd31db1bd
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-02-07 03:06:58 +0000

    This Update: PictureFeed.js seperated into component
    Just Did:
    Feeds are fully implemented, sizing is an issue
    
    Last Major Update:
    Feed Screen for videos fully works

--- components/main/Feeds/Feed.js
+++ components/main/Feeds/Feed.js
@@ -22,6 +22,7 @@ import VideoControls from "./VideosFeed/feedControl/components/VideoControls";
 import {B} from "./Shared_Objects/Bold";
 import ProfileTitle from "./Shared_Objects/ProfileTitle";
 import Caption from "./Shared_Objects/Caption";
+import PictureFeed from "./PictureFeed";
 
 
 const {height, width} = Dimensions.get('window');
@@ -209,34 +210,7 @@ class FeedScreen extends Component {
                                 <ActivityIndicator size="large" color="red"/>
                             </View>
                             :
-                            <View style={{flex: 1}}>
-
-                                <FlatList
-                                    style={{flex: 1}}
-                                    contentContainerStyle={{paddingTop: 25}}
-                                    data={this.state.picturesDataFetched}
-                                    horizontal={false}
-                                    scrollEventThrottle={20}
-                                    showsVerticalScrollIndicator={false}
-                                    onViewableItemsChanged={this.handlePicturesViewableItemsChanged}
-                                    viewabilityConfig={{itemVisiblePercentThreshold: 30, waitForInteraction: true}}
-                                    overScrollMode="never"
-                                    renderItem={({item}) => {
-                                        return (
-                                            <View style={{flex: 1, marginBottom: 20}}>
-                                                <ProfileTitle name={item.name} profilePicture={item.profile}/>
-
-                                                <Image source={{uri: item.downloadURL}}
-                                                       style={styles.picture(width, height)}/>
-
-                                                <Caption  name={item.name} caption={item.caption}  />
-
-                                            </View>
-                                        )
-                                    }}
-                                />
-
-                            </View>
+                            <PictureFeed data={this.state.picturesDataFetched} />
                         }
                     </ScrollView>
                 </SafeAreaView>

47630a5d2abc2095a606468e85b267e12a73d964
Tyrone Friday <tyrone.f2000@outlook.com>
2022-02-15 20:48:53 +0000

    This Update: Images in Feedscreen now fit appropriately
    Previous Updates:
    Comments
    Feed.js switcher now moves  inside the feedscreen when the user scrolls instead of always being at the top.

--- components/main/Feeds/PictureFeed.js
+++ components/main/Feeds/PictureFeed.js
@@ -6,11 +6,13 @@ import Caption from "./Shared_Objects/Caption";
 import Comment from "./Shared_Objects/Comment";
 import Likes_Count_Txt from "./Shared_Objects/Likes_Count_Txt";
 import View_All_Comments from "./Shared_Objects/View_All_Comments";
+
 const {height, width} = Dimensions.get('window');
 
 export default function PictureFeed(props) {
 
     return (
+        <View style={{flex: 1, marginBottom: 20}}>
             <FlatList
                 style={{flex: 1}}
                 contentContainerStyle={{paddingTop: 25}}
@@ -18,7 +20,7 @@ export default function PictureFeed(props) {
                 horizontal={false}
                 scrollEventThrottle={20}
                 showsVerticalScrollIndicator={false}
-               // onViewableItemsChanged={this.handlePicturesViewableItemsChanged}
+                // onViewableItemsChanged={this.handlePicturesViewableItemsChanged}
                 viewabilityConfig={{itemVisiblePercentThreshold: 30, waitForInteraction: true}}
                 overScrollMode="never"
                 renderItem={({item}) => {
@@ -27,21 +29,25 @@ export default function PictureFeed(props) {
                         <View style={{flex: 1, marginBottom: 20}}>
                             <ProfileTitle name={item.name}
                                           profilePicture={item.profile}
-                                          userID ={item.userID}
+                                          userID={item.userID}
                                           navigation={props.navigation}
                             />
 
                             <Image source={{uri: item.downloadURL}}
-                                   style={styles.picture(width, height)}/>
+                                   style={{ width: '100%',
+                                       height: undefined,
+                                       aspectRatio: 1,}}/>
 
-                            <Likes_Count_Txt likesCount={item.likesCount} navigation={props.navigation} />
+                            <Likes_Count_Txt likesCount={item.likesCount} navigation={props.navigation}/>
 
-                            <Caption  name={item.name}  userID={item.userID}  navigation={props.navigation}  caption={item.caption}/>
-                            <View_All_Comments  commentsCount={item.commentsCount} navigation={props.navigation}/>
+                            <Caption name={item.name} userID={item.userID} navigation={props.navigation}
+                                     caption={item.caption}/>
+                            <View_All_Comments commentsCount={item.commentsCount} navigation={props.navigation}/>
                         </View>
                     )
                 }}
             />
+        </View>
     )
 }
 

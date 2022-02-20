d981f1b5d889367742b4a58098d2b8b6d2cefda4
Tyrone Friday <tyrone.f2000@outlook.com>
2022-02-20 01:12:11 +0000

    In this Update:
    Likes & comment count number into csv
    
    Need To work On:
    Expanding view for comments and likes when the count is big
    implement likes system on videos feed
    
    Previous Updates:
    Refactoring
    Like button works for liking and unliking posts for PictureFeed.js
    Passing postID to comments & Likes BTN Objects
    Feed page implemented new DB for postData
    Implemented global styles sheet for feed
    Done - Need to implement GUi on videosFeed
    GUI looks how it needs to for PictureFeed.js
    Done - Configuring viewable BTN & TXT based on image background as its hard to see white Like txt on a white background.
    Like BTN Basics implemented
    Renamed variables from profilePicture to profileImage to match DB
    USernames added to userstories
    Usernames are also shortened if they are too long
    Username added to stories in feed
    Font size typo error in  <Username_Link_Txt>

--- components/main/Feeds/Shared_Objects/Likes_And_Comments/Likes_And_Comments_Count_Txt.js
+++ components/main/Feeds/Shared_Objects/Likes_And_Comments/Likes_And_Comments_Count_Txt.js
@@ -3,14 +3,38 @@ import React from "react";
 import {B} from "../Bold";
 
 export default function Likes_And_Comments_Count_Txt(props) {
+
+    const splitNumberByCommas = (value) =>{
+
+        console.log(`\nLikePost Value: ${value}`)
+
+        const stringVal = `${value}`;
+        const txt_Split = Array.from(stringVal);
+        const txtLength = txt_Split.length;
+
+        let  result = "";
+        let count = 0;
+
+        for (let i = txtLength-1; i >= 0; i--) {
+
+            console.log(`\nChar: ${txt_Split[i]}`)
+            if(count!= 0 && count % 3 === 0)
+            {
+                result = "," +result;
+            }
+            result = `${txt_Split[i]}`+result;
+            count++;
+        }
+        return result;
+    }
     return (
-        <Text style={{marginLeft: 10, color: 'white', fontSize: 15, fontWeight: 'bold'}}
+        <Text style={{marginLeft: 10, color: 'white', fontSize: 15, fontWeight: 'bold', paddingRight:10}}
               onPress={() => {
                   console.log(`\n\nGo to Likes Count Page`)
                   // props.navigation.navigate("PublicProfile", {uid: props.userID})
 
               }
               }>
-           <B>{props.count}</B></Text>
+           <B>{splitNumberByCommas(props.count)}</B></Text>
     )
 }

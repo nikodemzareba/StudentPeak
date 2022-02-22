eac89b7d9cadb1494e938fb5a36a289cc5b84cb5
Tyrone Friday <tyrone.f2000@outlook.com>
2022-02-22 02:21:45 +0000

    In this Update: Comments

--- components/main/Search.js
+++ components/main/Search.js
@@ -42,6 +42,8 @@ export default function Search(props) {
                 setUsers(tempData);
             })
     }
+
+
     return (
         <ScrollView style={{flex: 1, paddingTop: 15, backgroundColor: "black"}}>
             <View style={feedStyles.screenBackground}>
@@ -55,8 +57,15 @@ export default function Search(props) {
                 </View>
 
                 {/*
-                     Add another flatlist for searching
+                     Add another flatlist here for searching for posts with the tag the user inputted into the search text
+                     field - this will use the postData db and the tags fields on each post.
 
+                     The flatlist could generate a object with the potential result the db pulled and  a search icon on
+                     the far right (look at instagrams search).
+
+                     Once the user clicks on that object it could navigate the  user to another page and pass on the data 
+                     gathered from posts with that tag to this page which is Deji's job to display that info by flatlisting
+                     it.
 
                */}
                 <FlatList

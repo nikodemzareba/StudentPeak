be572aa3d4bcd29bbfb807149a177528d0d51123
Tyrone Friday <tyrone.f2000@outlook.com>
2022-02-22 01:59:33 +0000

    In this Update: .Integrated Search.js into new app design .Created New ProfileIcon_And_Username.js .Created Trending feeds basic page .Updated search screen to include profile icons and text
    Need To work On:
    .Switch (pictures / videos) overlaps topnotch of camera with certain phones
    
    Previous Updates:
    Fixed Bug - Chat Nav Icon disappeared
    Shows message on feed instead of blank screen if the user isnt following anyone.
    .Refactoring
    .Likes works on Videos page

--- components/main/Feeds/ProfileIcon_And_Username.js
+++ components/main/Feeds/ProfileIcon_And_Username.js
@@ -1,4 +1,4 @@
-import {View} from "react-native";
+import {TouchableOpacity, View} from "react-native";
 import Profile_Icon from "./Shared_Objects/Profile_Icon";
 import Username_Link_Txt from "./Shared_Objects/Username_Link_Txt";
 import {isUserNameTooLong} from "./Shared_Objects/FunctionsAndMethods/isUserNameTooLong";
@@ -15,18 +15,18 @@ export default function ProfileIcon_And_Username(props) {
 
 
         }}>
-            <View style={{flexDirection: 'row', alignItems: 'center', paddingRight:40}}>
+            <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', paddingRight:40}}>
                 <Profile_Icon userID={props.userID} width={50} height={50} borderRadius={50}
                               profileImage={props.profileImage} navigation={props.navigation}/>
-            </View>
-            <View style={{alignItems: 'flex-start'}}>
+            </TouchableOpacity>
+            <TouchableOpacity style={{alignItems: 'flex-start'}}>
                 <Username_Link_Txt
                     name={isUserNameTooLong(props.name, 28)}
                     userID={props.key}
                     fontSize={15}
                     fontWeight={'bold'} navigation={props.navigation}
                 />
-            </View>
+            </TouchableOpacity>
 
         </View>
     )
--- components/main/Search.js
+++ components/main/Search.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {View, Text, TextInput, FlatList, TouchableOpacity} from 'react-native'
+import {View, Text, TextInput, FlatList, TouchableOpacity, ScrollView} from 'react-native'
 import Topbar from './top/Topbar';
 import {connect} from 'redux';
 
@@ -7,6 +7,8 @@ import {connect} from 'redux';
 //import from firebase
 import firebase from 'firebase';
 import {B} from "./Feeds/Shared_Objects/Bold";
+import {feedStyles} from "./Feeds/Shared_Objects/Styles";
+import ProfileIcon_And_Username from "./Feeds/ProfileIcon_And_Username";
 
 require('firebase/firestore');
 
@@ -16,37 +18,67 @@ export default function Search(props) {
     console.log("\n\nheheree ")
 
     const fetchUsers = (search) => {
+        let tempData = [];
         firebase.firestore()
             .collection('users')
             .where('username', '>=', search)
             .get()
             .then((snapshot) => {
-                let users = snapshot.docs.map(doc => {
-                    const data = doc.data();
-                    const id = doc.id;
-                    return {id, ...data}
-                });
-                setUsers(users);
+
+                snapshot.forEach((userDetails) => {
+
+                    const userID = userDetails.id;
+                    const username = userDetails.get("username");
+                    const profileImage = userDetails.get("profileimage");
+
+                    tempData.push({
+                        key: userID,
+                        username: username,
+                        profileImage: profileImage,
+                    });
+
+                })
+
+                setUsers(tempData);
             })
     }
     return (
+        <ScrollView style={{flex: 1, paddingTop: 15, backgroundColor: "black"}}>
+            <View style={feedStyles.screenBackground}>
+                <View style={{ paddingTop: 10, height: 30}}>
+                </View>
 
-        <View style={{paddingTop:50}}>
-            <TextInput
-                placeholder="Search" onChangeText={(search) => fetchUsers(search)}/>
-            <FlatList
-                numColumns={1}
-                horizontal={false}
-                data={users}
-                renderItem={({item}) => (
-                    <TouchableOpacity
-                        onPress={() => props.route.params.navigation.navigate("PublicProfile", {uid: item.id})}>
-                        < Text>{item.username}</Text>
-                    </TouchableOpacity>
+                <View style={{backgroundColor: "white",  justifyContent:"center", height: 50}}>
+                    <TextInput
+                        placeholder="Search" onChangeText={(search) => fetchUsers(search)}
+                    />
+                </View>
 
-                )}
-            />
-        </View>
+                {/*
+                     Add another flatlist for searching
+
+
+               */}
+                <FlatList
+                    numColumns={1}
+                    horizontal={false}
+                    data={users}
+                    contentContainerStyle={{paddingTop: 25}}
+                    renderItem={({item}) => {
+
+                        return (
+                            <ProfileIcon_And_Username
+                                name={item.username}
+                                userID={item.key}
+                                profileImage={item.profileImage}
+                                navigation={props.navigation}
+                            />
+                        )
+                    }}
+                />
+            </View>
+
+        </ScrollView>
 
     )
 }

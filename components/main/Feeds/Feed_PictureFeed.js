35ecb7d82abfd75c3c224f3e68e6b5b8bc62b4c2
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2022-02-26 06:56:44 +0000

    In this Update: Refactoring & Creating Stories Objects
    Need to do:
    
    In Previous Update:
    .Changed LikeBTN  to a transaction update
    .Included Batched Update
    .Changed like button functionality due to an error on old data in the flatlist being rendered once the user switched tabs from pictures to videos tab.
    
    .Changed code to match new post DB structure

--- components/main/Feeds/Feed_PictureFeed.js
+++ components/main/Feeds/Feed_PictureFeed.js
@@ -5,7 +5,8 @@ import React, {useState} from "react";
 import {feedStyles} from "./Shared_Objects/Styles";
 
 import Pictures_And_Videos_Post_Object from "./Shared_Objects/Likes_And_Comments/Pictures_And_Videos_Post_Object";
-import StoriesObject from "./Shared_Objects/StoriesObject";
+import StoriesObject from "./Shared_Objects/Stories/StoriesObject";
+import Stories_FlatList from "./Shared_Objects/Stories/Stories_FlatList";
 
 
 
@@ -17,30 +18,32 @@ export default function Feed_PictureFeed(props) {
         <View style={feedStyles.screenBackground}>
 
             {/*Profile Stories */}
-            <FlatList
-                showsHorizontalScrollIndicator={false}
-                data={props.storyData}
-                horizontal
-                contentContainerStyle={{
-                    alignItems: 'center'
-                }}
-                renderItem={({item}) => {
-                    return (
+            {/*<FlatList*/}
+            {/*    showsHorizontalScrollIndicator={false}*/}
+            {/*    data={props.storyData}*/}
+            {/*    horizontal*/}
+            {/*    contentContainerStyle={{*/}
+            {/*        alignItems: 'center'*/}
+            {/*    }}*/}
+            {/*    renderItem={({item}) => {*/}
+            {/*        return (*/}
 
-                        <StoriesObject 
-                            userID={item.key}
-                            profileImage={item.profileImage}
-                            width={45}
-                            height={45}
-                            borderRadius={45}
-                            username={item.username}
-                            maxUsernameLength={8}
-                            fontSize={9}
-                            navigation={props.navigation}
-                        />
-                    )
-                }}
-            />
+            {/*            <StoriesObject */}
+            {/*                userID={item.key}*/}
+            {/*                profileImage={item.profileImage}*/}
+            {/*                width={45}*/}
+            {/*                height={45}*/}
+            {/*                borderRadius={45}*/}
+            {/*                username={item.username}*/}
+            {/*                maxUsernameLength={8}*/}
+            {/*                fontSize={9}*/}
+            {/*                navigation={props.navigation}*/}
+            {/*            />*/}
+            {/*        )*/}
+            {/*    }}*/}
+            {/*/>*/}
+
+            <Stories_FlatList   storyData={props.storyData} />
 
             {/*Picture Feed Posts */}
             <FlatList

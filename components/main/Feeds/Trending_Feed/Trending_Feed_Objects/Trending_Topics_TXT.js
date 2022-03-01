e9556cb624c16405f6d11ff9857ef969e4fc52f2
Tyrone Friday <tyrone.f2000@outlook.com>
2022-03-01 18:39:26 +0000

    In this Update: .Trending Topics on trending page links to SearchScreenResults.js
    In Previous Update:
    .Search Screen & SearchScreenResults() are displaying results
    .Feed Screen reloads when user goes to page.
    .TrendingFeeds page now reloads data each time the user navigates to the page.
    .avoid adding the same post liked by multiple users
    .trending page works for pictures  & Videos
    .trending page works for pictures
    .friendsLiked feed is up
    .trending feed is up
    .Orderby likecoutn works
    .Comments & Likes Display wasnt expecting with numbers for likes & count.
    .Explore feed can fit 1billion of each likes and comments
    .Trending Topic Number object shape now increases with text size
    .Navigational features are up on trending posts flatlist objects
    .GUi up
    .Refactoring
    .Created Trending Topics Object
    .Trending page gets users following liked posts
    .& Creating Stories Objects
    .Refactoring
    .getProfileImage method works

--- components/main/Feeds/Trending_Feed/Trending_Feed_Objects/Trending_Topics_TXT.js
+++ components/main/Feeds/Trending_Feed/Trending_Feed_Objects/Trending_Topics_TXT.js
@@ -1,25 +1,42 @@
 import {feedStyles} from "../../Shared_Objects/Styles";
-import {Text, View} from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
 import {B} from "../../Shared_Objects/Bold";
 import React from "react";
+import firebase from "firebase";
 
 
 export default function Trending_Topics_TXT(props) {
 
-    return(
-        <View style={feedStyles.trendingTopics}>
+    const checkIfThereAreAnyPosts =() =>{
+        firebase.firestore()
+            .collection('postTags')
+            .doc(props.text)
+            .get()
+            .then((postTag) =>{
 
-            <View style={feedStyles.trendingNumber}>
+                if(postTag.get("numberOfPosts") > 0)
+                {
+                    props.navigation.navigate("SearchScreenResults", {postTag: props.text})
+                    return
+                }
+                alert(`Sorry, the tag ${props.text} has no results`);
+            })
+
+    }
+    return(
+        <TouchableOpacity style={feedStyles.trendingTopics} onPress={checkIfThereAreAnyPosts}>
+
+            <TouchableOpacity style={feedStyles.trendingNumber}>
                 <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
                     <B> #{props.number}</B>
                 </Text>
-            </View>
+            </TouchableOpacity>
 
 
             <Text style={{marginLeft: 10, color: 'black', fontSize: 18, fontWeight: 'bold', paddingRight: 10}}>
                 <B>{props.text}</B>
             </Text>
 
-        </View>
+        </TouchableOpacity>
     )
 }

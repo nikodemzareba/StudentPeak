18bb2f9b2844b105bdc3d7be0a7fe0297bffd867
Tyrone Friday <tyrone.f2000@outlook.com>
2022-03-01 23:02:37 +0000

    In this Update: .Video thumbnail - fixed issues with video thumbnail not working
    Need to do:
    
    In Previous Update:
    .Sizing of video and picture of on individual post works ViewPost.js
    .Refactoring
    .Explore page works
    .deleted old search screen
    . SearchScreenResults() - update (search bar at the top)
    .Trending Topics on trending page links to SearchScreenResults.js
    .Search Screen & SearchScreenResults() are displaying results
    .Feed Screen reloads when user goes to page.
    .TrendingFeeds page now reloads data each time the user navigates to the page.
    .avoid adding the same post liked by multiple users
    .trending page works for pictures  & Videos
    .trending page works for pictures
    .friendsLiked feed is up
    .trending feed is up
    .Orderby likecoutn works
    .Comments & Likes Display wasnt expecting with numbers for likes & count.
    .Explore feed can fit 1billion of each likes and comments
    .Trending Topic Number object shape now increases with text size
    .Navigational features are up on trending posts flatlist objects
    .GUi up
    .Refactoring
    .Created Trending Topics Object
    .Trending page gets users following liked posts
    .& Creating Stories Objects
    .Refactoring
    .getProfileImage method works

--- components/main/Search/Objects/SearchScreenObject.js
+++ components/main/Search/Objects/SearchScreenObject.js
@@ -1,9 +1,41 @@
-import {Image, StyleSheet, TouchableOpacity} from "react-native";
+import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
 
-import React from "react";
-import {Video} from "expo-av";
+import React, {useEffect, useState} from "react";
+import * as VideoThumbnails from 'expo-video-thumbnails';
 
 export default function SearchScreenObject(props) {
+
+    const [doneThumbnail, setDoneThumbnail] = useState(false);
+    const [videoImageThumbnail, setVideoImageThumbnail] = useState(null);
+
+    const generateThumbnail = async (uriIn) => {
+        console.log(`\n\nSearchScreenObject generateThumbnail() URI itemKEy \n${props.item.key}`);
+        try {
+            const {uri} = await VideoThumbnails.getThumbnailAsync(
+                uriIn,
+                {
+                    time: 15000,
+                }
+            );
+
+            console.log(`\n\nSearchScreenObject generateThumbnail() URI \n${uri}`);
+
+            setVideoImageThumbnail(uri);
+            setDoneThumbnail(true);
+
+
+        } catch (e) {
+            console.log(`\n\nSearchScreenObject generateThumbnail() Error \n${e}`);
+        }
+    };
+
+    useEffect(() => {
+        if (props.item.mediaType === 'video') {
+            generateThumbnail(props.item.downloadURL);
+        }
+    }, []);
+
+
     return (
         <TouchableOpacity style={{flex: 1, flexDirection: 'column', margin: 1}}
                           onPress={() => {
@@ -16,7 +48,18 @@ export default function SearchScreenObject(props) {
                 <Image style={styles.imageThumbnail} source={{uri: props.item.downloadURL}}/>
                 :
 
-                <Image style={styles.imageThumbnail} source={{uri: props.item.downloadURL}}/>}
+                <View>
+                    {doneThumbnail
+                        ?
+                        <Image style={styles.imageThumbnail} source={{uri: videoImageThumbnail}}/>
+                        :
+
+                        <Image style={{ width:135, height:100, alignSelf:"center"}} source={require('../Pictures/youtube-logo2.jpg')}/>
+
+                    }
+
+                </View>
+            }
 
         </TouchableOpacity>
     )

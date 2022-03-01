7c8dd3ef6adf7b189c0e9617560f1fe00a0c510f
Tyrone Friday <tyrone.f2000@outlook.com>
2022-03-01 18:39:16 +0000

    In this Update: .Trending Topics on trending page links to SearchScreenResults.js
    In Previous Update:
    .Search Screen & SearchScreenResults() are displaying results
    .Feed Screen reloads when user goes to page.
    .TrendingFeeds page now reloads data each time the user navigates to the page.
    .avoid adding the same post liked by multiple users
    .trending page works for pictures  & Videos
    .trending page works for pictures
    .friendsLiked feed is up
    .trending feed is up
    .Orderby likecoutn works
    .Comments & Likes Display wasnt expecting with numbers for likes & count.
    .Explore feed can fit 1billion of each likes and comments
    .Trending Topic Number object shape now increases with text size
    .Navigational features are up on trending posts flatlist objects
    .GUi up
    .Refactoring
    .Created Trending Topics Object
    .Trending page gets users following liked posts
    .& Creating Stories Objects
    .Refactoring
    .getProfileImage method works

--- components/main/Search/Objects/Find_Post_Object.js
+++ components/main/Search/Objects/Find_Post_Object.js
@@ -4,74 +4,24 @@ import firebase from "firebase";
 
 export default function Find_Post_Object(props) {
 
-    const getPosts = () => {
-
-        // Get the posts related to the tag (results = postID's)
+    const checkIfThereAreAnyPosts =() =>{
         firebase.firestore()
             .collection('postTags')
             .doc(props.postTag)
-            .collection('posts')
             .get()
-            .then((posts) =>{
+            .then((postTag) =>{
 
-                const resultSize = posts.size;
-                let count =0;
-                let postsData =[]
+                if(postTag.get("numberOfPosts") > 0)
+                {
+                    props.navigation.navigate("SearchScreenResults", {postTag: props.postTag})
 
-                // For each postID
-                posts.forEach((post) =>{
-
-                    // For each postID get its data
-                    firebase.firestore()
-                        .collection('postData')
-                        .doc(post.id)
-                        .get()
-                        .then((postData) => {
-
-                            count++;
-
-                            // Get the user who posted the post's DATA
-                            firebase.firestore()
-                                .collection('users')
-                                .doc(postData.get("userID"))
-                                .get()
-                                .then(userDetails => {
-
-                                    const profileImage = userDetails.get("profileimage");
-                                    const username = userDetails.get("username");
-                                    const userID = postData.get("userID");
-
-                                    const caption = postData.get("caption");
-                                    const createdAt = postData.get("createdAt");
-                                    const downloadURL = postData.get("downloadURL");
-                                    const mediaType = postData.get("mediaType");
-
-                                    const commentsCount = postData.get("commentsCount");
-
-                                    postsData.push({
-                                        key: postData.id,
-                                        userID: userID,
-                                        name: username,
-                                        profile: profileImage,
-                                        caption: caption,
-                                        createdAt: createdAt,
-                                        downloadURL: downloadURL,
-                                        mediaType: mediaType,
-
-                                        commentsCount: commentsCount, // Needs be retrieved inside the comment method
-                                    });
-
-                                    if(count===resultSize)
-                                    {
-                                        const data = postsData;
-                                        props.navigation.navigate("SearchScreenResults", {postTag: props.postTag, data:data})
-                                    }
-                                })
-                        })
-                })
-
+                }
+                else {
+                    alert(`Sorry, the tag ${props.postTag} has no results`);
+                }
 
             })
+
     }
 
     return (
@@ -82,7 +32,7 @@ export default function Find_Post_Object(props) {
             paddingHorizontal: 10,
         }}
 
-                          onPress={getPosts}
+                          onPress={checkIfThereAreAnyPosts}
         >
             <Image
                 source={require('../Objects/SearchIcon.png')}
--- components/main/Search/SearchScreenResults.js
+++ components/main/Search/SearchScreenResults.js
@@ -1,12 +1,87 @@
 import {ScrollView, View, Text} from "react-native";
 import React, {useEffect, Component, useState} from 'react'
 import {feedStyles} from "../Feeds/Shared_Objects/Styles";
+import firebase from "firebase";
 
 export default function SearchScreenResults(props) {
 
+    const getPosts = () => {
+
+        // Get the posts related to the tag (results = postID's)
+        firebase.firestore()
+            .collection('postTags')
+            .doc(props.postTag)
+            .collection('posts')
+            .get()
+            .then((posts) =>{
+
+                const resultSize = posts.size;
+                let count =0;
+                let postsData =[]
+
+                // For each postID
+                posts.forEach((post) =>{
+
+                    // For each postID get its data
+                    firebase.firestore()
+                        .collection('postData')
+                        .doc(post.id)
+                        .get()
+                        .then((postData) => {
+
+                            count++;
+
+                            // Get the user who posted the post's DATA
+                            firebase.firestore()
+                                .collection('users')
+                                .doc(postData.get("userID"))
+                                .get()
+                                .then(userDetails => {
+
+                                    const profileImage = userDetails.get("profileimage");
+                                    const username = userDetails.get("username");
+                                    const userID = postData.get("userID");
+
+                                    const caption = postData.get("caption");
+                                    const createdAt = postData.get("createdAt");
+                                    const downloadURL = postData.get("downloadURL");
+                                    const mediaType = postData.get("mediaType");
+
+                                    const commentsCount = postData.get("commentsCount");
+
+                                    postsData.push({
+                                        key: postData.id,
+                                        userID: userID,
+                                        name: username,
+                                        profile: profileImage,
+                                        caption: caption,
+                                        createdAt: createdAt,
+                                        downloadURL: downloadURL,
+                                        mediaType: mediaType,
+
+                                        commentsCount: commentsCount, // Needs be retrieved inside the comment method
+                                    });
+
+                                    console.log(`\nFind_Post_Object() \nUserID: ${userID} \nUserName: ${username} \nProfile Picture: ${profileImage}   \nPostID : ${postData.id} \nMediaType : ${mediaType} \nCaption: ${caption} \nCreatedAt: ${createdAt} \nDownloadURL: ${downloadURL} \nMediaType: ${mediaType} \nCommentsCount: ${commentsCount} `);
+
+                                    if(count===resultSize)
+                                    {
+                                        const data = postsData;
+
+                                    }
+                                })
+                        })
+                })
+
+
+            })
+    }
+
+
+    console.log(`\n\nSearchScreenResults() \n${props.route.params.postTag}`)
     if(props.data !== undefined) {
         props.data.foreach((item) => {
-            console.log(`\n\n${item.id}`)
+            console.log(`\n\n${item.key}`)
         })
     }
 

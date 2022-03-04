16e767d2d87d0a16484633b5bfee569f3f7eaabb
Tyrone Friday <tyrone.f2000@outlook.com>
2022-03-04 01:45:44 +0000

    In this Update: .Refactoring
    Need to do:
    
    In Previous Update:

--- components/main/PrivateProfile.js
+++ components/main/PrivateProfile.js
@@ -1,30 +1,18 @@
-import React, {Component, useRef, useState} from 'react'
+import React, {Component} from 'react'
 import {
     View,
-    Dimensions,
-    Text,
     ActivityIndicator,
     ScrollView,
     StyleSheet,
-    Image, TouchableOpacity, FlatList
+    FlatList
 } from 'react-native';
 
 import firebase from "firebase";
-import Feed_PictureFeed from "./Feeds/Feed_PictureFeed";
-import Feed_VideoFeed from "./Feeds/Feed_VideoFeed";
 
-
-const {height, width} = Dimensions.get('window');
-
-
-const separator = "###########################################################################################";
 
 import {storyData} from "./Feeds/FakeJSONData/TempStoryData";
-import {B} from "./Feeds/Shared_Objects/Bold";
 import PrivateProfileDisplay from './PrivateProfileDisplay';
-import Pictures_And_Videos_Post_Object from "./Feeds/Shared_Objects/Pictures_And_Videos_Post_Object";
 import {feedStyles} from "./Feeds/Shared_Objects/Styles";
-import {SimpleLineIcons} from "@expo/vector-icons";
 import SearchScreenObject from "./Search/Objects/SearchScreenObject";
 
 
@@ -35,18 +23,14 @@ class PrivateProfile extends Component {
 
         this.state = {
 
-            storiesData: [],
-            storiesDataLoaded: false,
-
             profileImageLoaded: false,
             profileImage: "",
             userId:
-            // "upb6UG9eM0VWzRo8tGke3xK9p953",
             firebase.auth().currentUser.uid,
 
-            picturesDataFetched: [],
-            picturesIsLoading: true,
-            loadPictures: false,
+            mediaDataDataFetched: [],
+            mediaDataIsLoading: true,
+            loadMediaData: false,
         }
         this.usersFollowingRef = firebase.firestore()
             .collection('following')
@@ -69,8 +53,7 @@ class PrivateProfile extends Component {
     // This method is passed all the userID's of the users this user is following
     getData = async () => {
 
-        const picturesDataFetched = [];
-
+        const mediaDataDataFetched = [];
 
         // Got users Following info
         // console.log("\nGot Users Following Data")
@@ -80,7 +63,6 @@ class PrivateProfile extends Component {
             .get()
             .then(userDetails => {
 
-
                 // Get all of the posts from the user we are following
                 firebase.firestore()
                     .collection('posts')
@@ -88,13 +70,9 @@ class PrivateProfile extends Component {
                     .collection('userPosts')
                     .get()
                     .then(privatePosts => {
-                        //     console.log("\nGot Posts Of Users i am Following!")
-
-                        // For each post from the user we are following
-
 
                         let expectedResultsSize = privatePosts.size;
-                        let count =0;
+                        let count = 0;
 
                         privatePosts.forEach((userPost) => {
 
@@ -112,29 +90,24 @@ class PrivateProfile extends Component {
                                     const createdAt = postData.get("createdAt");
                                     const downloadURL = postData.get("downloadURL");
                                     const mediaType = postData.get("mediaType");
-                                    const commentsCount = postData.get("commentsCount");
-                                    const likesCount = postData.get("likesCount");
                                     const userID = this.state.userId;
 
-
-                                    picturesDataFetched.push({
+                                    mediaDataDataFetched.push({
                                         key: userPost.id,
                                         caption: caption,
                                         createdAt: createdAt,
                                         downloadURL: downloadURL,
                                         mediaType: mediaType,
-                                        commentsCount: commentsCount, // Needs be retrieved inside the comment method
                                         profile: profileImage,
                                         name: name,
                                         userID: userID,
                                     });
 
-                                    if(count===expectedResultsSize)
-                                    {
+                                    if (count === expectedResultsSize) {
                                         //       console.log("\nSetting Data To Variable")
                                         this.setState({
-                                            picturesDataFetched: picturesDataFetched,
-                                            loadPictures: false
+                                            mediaDataDataFetched: mediaDataDataFetched,
+                                            loadMediaData: false
                                         })
                                     }
                                 }))
@@ -147,8 +120,6 @@ class PrivateProfile extends Component {
                         //       console.log(`${error} \nUnable to get Users following posts!`);
                     })
             });
-
-
     }
 
     getProfileInfo = async () => {
@@ -169,7 +140,7 @@ class PrivateProfile extends Component {
                 const username = userInfo.get("username");
                 const profileimage = userInfo.get("profileimage");
 
-                
+
                 profileDataFetched.push({
                     key: this.state.userId,
                     bio: bio,
@@ -202,8 +173,8 @@ class PrivateProfile extends Component {
                 </View>
 
 
-                {/* Pictures Feed */}
-                {this.state.loadPictures
+                {/* mediaData Feed */}
+                {this.state.loadMediaData
                     ?
                     <View style={styles.loading}>
                         <ActivityIndicator size="large" color="red"/>
@@ -216,11 +187,15 @@ class PrivateProfile extends Component {
                             </View>
 
                             <FlatList
-                                data={this.state.picturesDataFetched}
-                                //Setting the number of column
+                                data={this.state.mediaDataDataFetched}
                                 numColumns={3}
                                 renderItem={({item}) => (
-                                    <SearchScreenObject item={item} navigation={this.props.route.params.navigation} comingFrom={"PrivateProfile"}/>
+
+                                    <SearchScreenObject
+                                        item={item}
+                                        navigation={this.props.route.params.navigation}
+                                        comingFrom={"PrivateProfile"}
+                                    />
                                 )}
 
                             />
@@ -244,24 +219,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    video: (width, height) => ({
-        alignSelf: 'center',
-        width: width,
-        height: height
-    }),
-    picture: (width, height) => ({
-        alignSelf: 'center',
-        width: width,
-        height: height
-    }),
-    container: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-    controlsContainer: {
-        position: 'absolute',
-        bottom: 10
-    }
 })
 
 export default PrivateProfile;
